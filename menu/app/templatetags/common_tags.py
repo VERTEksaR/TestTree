eb790fd36c54b419e1eb320539ea1e394d818cc1
Valeriy Chekavyi <14.vert.41@gmail.com>
2024-07-30 01:17:29 +0300

    Initial commit

--- menu/app/templatetags/common_tags.py
+++ menu/app/templatetags/common_tags.py
@@ -8,15 +8,15 @@ register = template.Library()
 @register.inclusion_tag('menu.html')
 def draw_menu(name_menu):
     """Функция по созданию меню"""
-    alla = []
+    list_of_units = []
     item = Menu.objects.filter(url_name=name_menu).select_related('parent')[0]
-    check_children(item, alla)
-    return {"items": alla}
+    check_children(item, list_of_units)
+    return {"items": list_of_units}
 
 
-def check_children(aaa, alla):
+def check_children(unit, list_of_units):
     """Функция для добавления всех элементов меню в единый список"""
-    alla.append(aaa)
-    if aaa.children.exists():
-        for gg in aaa.children.all():
-            check_children(gg, alla)
+    list_of_units.append(unit)
+    if unit.children.exists():
+        for item in unit.children.all():
+            check_children(item, list_of_units)
